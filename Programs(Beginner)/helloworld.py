dd32841568e9aca7ee9fba38cddb157dd133a98d
devIncogni <arinkumar10@outlook.com>
2024-02-19 22:55:25 +0530

    hellowworld edits

--- Programs(Beginner)/helloworld.py
+++ Programs(Beginner)/helloworld.py
@@ -1,1 +1,22 @@
 print("Hello World")
+print(17*13)
+# This is a comment
+print("This is line 1\nThis is line 2") # This is another comment
+"""
+This is a multiline 
+comment
+with double
+quotes
+
+"""
+
+'''
+This is multiline
+comment
+with 
+single quotes
+
+'''
+
+print("Using doubl quotes \"inside print double quotes\" and slash\\ ")
+print("Multiple values and separator ~, ending 6969", 6, 7, sep="~", end="\n969")
